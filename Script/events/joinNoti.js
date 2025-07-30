4d556bf57af9a36fed58869f9dafc163b4a3907d
Tawsif Ahmed <97363372+TawsifAhmed143@users.noreply.github.com>
2025-07-30 15:46:26 +0800

    Update joinNoti.js

--- Script/events/joinNoti.js
+++ Script/events/joinNoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "joinNoti",
     eventType: ["log:subscribe"],
     version: "1.0.1",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "MD SABBIR ☢️_𖣘 -𝐁𝐎𝐓 ☢️",
     description: "Notification of bots or people entering groups with random gif/photo/video",
     dependencies: {
         "fs-extra": "",
