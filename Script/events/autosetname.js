6783efe4941e321c6de24f3928b4ddb12e2a7690
Tawsif Ahmed <97363372+TawsifAhmed143@users.noreply.github.com>
2025-07-30 15:48:57 +0800

    Update autosetname.js

--- Script/events/autosetname.js
+++ Script/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "MD SABBIR ☢️_𖣘 -𝐁𝐎𝐓 ☢️",
 	description: "Automatically set new member nicknames"
 };
 
